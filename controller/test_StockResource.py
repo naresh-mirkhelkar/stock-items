7e8794f7c97e738d68f4d7dca3057667165f81b7
nxm5777 <naresh_mirkhelkar@homedepot.com>
2020-04-18 16:23:38 -0400

    some changes

--- controller/test_StockResource.py
+++ controller/test_StockResource.py
@@ -9,24 +9,29 @@ import cv2
 
 class TestStockResource(unittest.TestCase):
 
+
+    @classmethod
+    def setUpClass(cls):
+        cls.tester = sr.app.test_client()
+
     def test_healthcheck(self):
         result = sr.healthcheck()
         self.assertEqual(result, 'Yes, service is healthy!')
 
     def test_health_using_service(self):
-        tester = sr.app.test_client()
-        response = tester.get('/api/healthcheck', content_type='html/text')
+        # tester = sr.app.test_client()
+        response = self.tester.get('/api/healthcheck', content_type='html/text')
         print(response.data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'Yes, service is healthy!')
 
     def test_object_detection_using_service(self):
-        tester = sr.app.test_client()
+        # tester = sr.app.test_client()
         content_type = 'image/jpeg'
         headers = {'content-type': content_type}
         img = cv2.imread('test-images/no-bottle.jpeg')
         _, img_encoded = cv2.imencode('.jpg', img)
-        response = tester.post('/api/processimage?filename=no-bottle.jpeg', data=img_encoded.tostring(), headers=headers)
+        response = self.tester.post('/api/processimage?filename=no-bottle.jpeg', data=img_encoded.tostring(), headers=headers)
         print(response.data)
         self.assertEqual(response.status_code, 200)
         self.assertIsNotNone(response.data)
---
+++ controller/test_StockServices.py
@@ -0,0 +1,6 @@
+import unittest
+import cv2
+
+class Test_StockServices(unittest.TestCase):
+
+    # def test_imageclassfiication(self):
